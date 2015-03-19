78fd6085f60bbf77b9ff2536218638fc727b8e13
Liu Ming <extrafliu@gmail.com>
2015-03-19 17:46:19 +0800

    init

---
+++ ole.go
@@ -0,0 +1,129 @@
+package ole2
+
+import (
+	"encoding/binary"
+	"io"
+)
+
+var ENDOFCHAIN = uint32(0xFFFFFFFE) //-2
+var FREESECT = uint32(0xFFFFFFFF)   // -1
+
+type Ole struct {
+	header   *Header
+	Lsector  uint32
+	Lssector uint32
+	SecID    []uint32
+	SSecID   []uint32
+	Files    []File
+	bts      []byte
+}
+
+func Open(bts []byte, charset string) (ole *Ole, err error) {
+	var header *Header
+	if header, err = parseHeader(bts[:512]); err == nil {
+		ole = new(Ole)
+		ole.bts = bts
+		ole.header = header
+		ole.Lsector = 512 //TODO
+		ole.Lssector = 64 //TODO
+		ole.bts = bts
+		ole.readMSAT()
+		return ole, nil
+	}
+
+	return nil, err
+}
+
+func (o *Ole) ListDir() (dir []*File, err error) {
+	sector := o.stream_read(o.header.Dirstart, 0)
+	dir = make([]*File, 0)
+	for {
+		d := new(File)
+		err = binary.Read(sector, binary.LittleEndian, d)
+		if err == nil && d.Type != EMPTY {
+			dir = append(dir, d)
+		} else {
+			break
+		}
+
+	}
+
+	return
+}
+
+func (o *Ole) OpenFile(file *File) io.ReadSeeker {
+	if file.Size < o.header.Sectorcutoff {
+		return o.short_stream_read(file.Sstart, file.Size)
+	} else {
+		return o.stream_read(file.Sstart, file.Size)
+	}
+}
+
+// Read MSAT
+func (o *Ole) readMSAT() {
+	// int sectorNum;
+
+	count := uint32(109)
+	if o.header.Cfat < 109 {
+		count = o.header.Cfat
+	}
+
+	for i := uint32(0); i < count; i++ {
+		sector := o.sector_read(o.header.Msat[i])
+		sids := sector.AllValues(o.Lsector)
+
+		o.SecID = append(o.SecID, sids...)
+	}
+
+	for sid := o.header.Difstart; sid != ENDOFCHAIN; {
+		sector := o.sector_read(sid)
+		sids := sector.MsatValues(o.Lsector)
+
+		for _, sid := range sids {
+			sector := o.sector_read(sid)
+			sids := sector.AllValues(o.Lsector)
+
+			o.SecID = append(o.SecID, sids...)
+		}
+
+		sid = sector.NextSid(o.Lsector)
+	}
+
+	for i := uint32(0); i < o.header.Csfat; i++ {
+		sid := o.header.Sfatstart
+
+		if sid != ENDOFCHAIN {
+			sector := o.sector_read(sid)
+
+			sids := sector.MsatValues(o.Lsector)
+
+			o.SSecID = append(o.SSecID, sids...)
+
+			sid = sector.NextSid(o.Lsector)
+		}
+	}
+
+}
+
+func (o *Ole) stream_read(sid uint32, size uint32) *StreamReader {
+	return &StreamReader{o.SecID, sid, o, sid, 0, o.Lsector, int64(size), 0}
+}
+
+func (o *Ole) short_stream_read(sid uint32, size uint32) *StreamReader {
+	return &StreamReader{o.SSecID, sid, o, sid, 0, o.Lssector, int64(size), 0}
+}
+
+func (o *Ole) sector_read(sid uint32) Sector {
+	pos := o.sector_pos(sid, o.Lsector)
+	bts := o.bts[pos : pos+o.Lsector]
+	return Sector(bts)
+}
+
+func (o *Ole) short_sector_read(sid uint32) Sector {
+	pos := o.sector_pos(sid, o.Lssector)
+	return Sector(o.bts[pos : pos+o.Lssector])
+}
+
+func (o *Ole) sector_pos(sid uint32, size uint32) uint32 {
+	return 512 + sid*size
+}
---
+++ stream_reader.go
@@ -0,0 +1,80 @@
+package ole2
+
+import (
+	"io"
+	"log"
+)
+
+var DEBUG = false
+
+type StreamReader struct {
+	sat              []uint32
+	start            uint32
+	ole              *Ole
+	offset_of_sector uint32
+	offset_in_sector uint32
+	size_sector      uint32
+	size             int64
+	offset           int64
+}
+
+func (r *StreamReader) Read(p []byte) (n int, err error) {
+	if r.offset_of_sector == ENDOFCHAIN {
+		return 0, io.EOF
+	}
+	for i := 0; i < len(p); i++ {
+		if r.offset_in_sector >= r.size_sector {
+			r.offset_in_sector = 0
+			r.offset_of_sector = r.sat[r.offset_of_sector]
+			if r.offset_of_sector == ENDOFCHAIN {
+				return i, io.EOF
+			}
+		}
+		pos := r.ole.sector_pos(r.offset_of_sector, r.size_sector) + r.offset_in_sector
+		p[i] = r.ole.bts[pos]
+		if DEBUG {
+			log.Printf("pos:%x,bit:%x", pos, p[i])
+		}
+		r.offset_in_sector++
+		r.offset++
+		if r.offset == int64(r.size) {
+			return i + 1, io.EOF
+		}
+	}
+	return len(p), nil
+}
+
+func (r *StreamReader) Seek(offset int64, whence int) (offset_result int64, err error) {
+
+	if whence == 0 {
+		r.offset_of_sector = r.start
+		r.offset_in_sector = 0
+		r.offset = offset
+	} else {
+		r.offset += offset
+	}
+
+	if r.offset_of_sector == ENDOFCHAIN {
+		return r.offset, io.EOF
+	}
+
+	for offset >= int64(r.size_sector-r.offset_in_sector) {
+		r.offset_of_sector = r.sat[r.offset_of_sector]
+		offset -= int64(r.size_sector - r.offset_in_sector)
+		r.offset_in_sector = 0
+		if r.offset_of_sector == ENDOFCHAIN {
+			err = io.EOF
+			goto return_res
+		}
+	}
+
+	if r.size <= r.offset {
+		err = io.EOF
+		r.offset = r.size
+	} else {
+		r.offset_in_sector += uint32(offset)
+	}
+return_res:
+	offset_result = r.offset
+	return
+}
